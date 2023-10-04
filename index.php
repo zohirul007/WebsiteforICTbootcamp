510c1399665401e32359f042d0b5131297d14578
Zohirul Haque <zhremon@outlook.com>
2023-10-04 23:53:54 +0600

    adding bootstrap icons

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Option 1: Include in HTML -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
     <link rel="stylesheet" href="style.css">
     <title>Prism project</title>
 </head>
@@ -80,9 +83,20 @@
     <section class="p-5">
         <div class="container">
             <div class="row text-center">
-                <div class="col">123</div>
-                <div class="col">123</div>
-                <div class="col">123</div>
+                <div class="col-md">
+                    <div class="card bg-dark text-light">
+                    <div class="card-body text-center">
+                        <div class="h1 mb-3">
+                        <i class="bi bi-laptop"></i>
+                        </div>
+                        <h3 class="card-title">
+                            Virtual
+                        </h3>
+                    </div>
+                 </div>
+                </div>
+                <div class="col-md">123</div>
+                <div class="col-md">123</div>
             </div>
         </div>
     </section>
